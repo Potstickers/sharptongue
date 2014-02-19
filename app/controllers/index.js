a4c6aa0ddaaf376d40220f959153b3e0d3b293fb
Wellington Zhao <no.c.l.unit@gmail.com>
2014-02-19 10:47:48 -0500

    logan stuff

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,3 +5,51 @@ exports.render = function(req, res) {
         user: req.user ? JSON.stringify(req.user) : 'null'
     });
 };
+
+exports.api =  function(text, lang) {
+    function translate(lang, text) {
+    	return "/translate_a/t?client=t&sl=auto&tl=" + lang + "&hl=en&sc=2&ie=UTF-8&oe=UTF-8&uptl=" + lang + "&ssel=0&tsel=0&q=" + text;
+    };
+    
+    function tts(lang, text) {
+    	return "/translate_tts?ie=UTF-8&q=" + text + "&tl=" + lang;
+    };
+    
+  	res.setHeader("Content-Type", "text/json; charset=utf-8");
+	var lang = req.params.lang, text = req.params.text;
+	http.get({
+		host: "translate.google.com",
+		port: 80,
+		path: translate(lang, text)
+	}, function(socket){
+		socket.on("data", function(chunk) {
+			chunk += "";
+			chunk = JSON.parse(chunk.substring(2, chunk.indexOf("]") + 1));
+			var body = {
+				output : chunk[0],
+				input : chunk[1],
+				pronunciation : chunk[2] || chunk[0],
+				tts : tts(lang, text),
+				translate : translate(lang, text)
+			};
+			http.get({
+				host: "translate.google.com",
+				port: 80,
+				path: tts(lang, body.pronunciation)
+			}, function(socket){
+				socket.on("data", function(data) {
+					body.audio = "data:audio/mp3;base64," + new Buffer(data).toString("base64");
+					res.send(body);
+				});
+			}).on("error", function(e){
+				send({
+					error : "Invalid API call (2)."
+				});
+			});
+		});
+	}).on("error", function(e){
+		send({
+			error : "Invalid API call (1)."
+		});
+	});
+};
