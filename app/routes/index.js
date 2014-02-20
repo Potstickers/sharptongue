554e877405988cbc39ce407f8bddbdd0af0c0646
Wellington Zhao <no.c.l.unit@gmail.com>
2014-02-20 10:32:42 -0500

    Fix regex err

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,5 @@ module.exports = function(app) {
     // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
-    //app.get('/translate/:text({1,})/to/:lang([a-z]{2})', index.api);
+    app.get('/translate/:text(.{1,})/to/:lang([a-z]{2})', index.api);
 };
