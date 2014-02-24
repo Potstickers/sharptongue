7077784374cc60831c759cffdb86997f53d7a10e
Wellington Zhao <no.c.l.unit@gmail.com>
2014-02-24 10:54:40 -0500

    update links in navbar

--- config/env/all.js
+++ config/env/all.js
@@ -12,6 +12,8 @@ module.exports = {
 	// The secret should be set to a non-guessable string that
 	// is used to compute a session hash
 	sessionSecret: 'th1sIstotallyN0nG1_!e5a13|e:)um4dBraH?umad?yeaumad...',
+	// Wow guys
+	
 	// The name of the MongoDB collection to store sessions in
 	sessionCollection: 'sessions'
 };
