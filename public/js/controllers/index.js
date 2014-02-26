efe662d58dad7483c9d8f5fe88d6a15bbf7d1f5e
Wellington Zhao <no.c.l.unit@gmail.com>
2014-02-26 10:04:44 -0500

    Test client calling service

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -2,4 +2,20 @@
 
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
+    $scope.fetchTTS = function() {
+      console.log('clicked');
+    	var myTranslate = document.getElementById("translate").value;
+        if (myTranslate !== null) {
+          $http({
+            url: 'translate/quick/to/es',
+              method : 'GET'
+            }).success(function(data) {
+              if (data) {
+                var speech = new Audio(data.tts);
+                speech.play();
+                console.log(data);
+              }
+          });
+      }
+    };
 }]);
