8cb181c66c71950c13b6d6af5974ff8314e0e3d3
Wellington Zhao <tuc16961@temple.edu>
2014-02-28 11:20:28 -0500

    Moved dom manip into directive

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,19 +4,6 @@ angular.module('sharptung.system').controller('IndexController',
   ['$scope', '$routeParams', '$http', 'Global', 'Translate',
   function ($scope, $routeParams, $http, Global, Translate) {
     $scope.global = Global;
-    //move into directive later
-    $scope.translateClicked = function() {
-      var text = angular.element(document.querySelector('#translate')).val();
-      console.log(text);
-      Translate.get({
-        text: text,
-        lang: 'es'
-      }, function(res) {
-        var speech = new Audio(res.audio);
-        speech.play();
-        console.log(res);
-      });
-    };
     $scope.translate = function(params) {
       Translate.get({
         text: params.text,
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,13 @@
 'use strict';
+
+angular.module('shartung.system').directive('translateClick', function() {
+  return function(scope, elem, attrs) {
+    elem.bind('click', function () {
+      var text = elem.val();
+      scope.translate({
+        text: text,
+        lang: 'es'
+      });
+    });
+  };
+});
