b3b6725ed77e98fbe9c8f237c831e6cca9d9e452
Wellington Zhao <tuc16961@temple.edu>
2014-02-28 10:28:59 -0500

    Add dom manip

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -8,8 +8,8 @@ angular.module('sharptung.system').controller('IndexController',
     $scope.translateClicked = function() {
       var text = angular.element(document.querySelector('#translate')).val();
       Translate.get({
-        text: params.text,
-        lang: params.lang
+        text: text,
+        lang: 'es'
       }, function(res) {
         var speech = new Audio(res.audio);
         speech.play();
