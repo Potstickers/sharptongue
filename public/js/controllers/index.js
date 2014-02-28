5c92b342715499499a563ef5cb58e476c2e449bb
Wellington Zhao <tuc16961@temple.edu>
2014-02-28 10:28:21 -0500

    Add dom manip

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,6 +4,18 @@ angular.module('sharptung.system').controller('IndexController',
   ['$scope', '$routeParams', '$http', 'Global', 'Translate',
   function ($scope, $routeParams, $http, Global, Translate) {
     $scope.global = Global;
+    //move into directive later
+    $scope.translateClicked = function() {
+      var text = angular.element(document.querySelector('#translate')).val();
+      Translate.get({
+        text: params.text,
+        lang: params.lang
+      }, function(res) {
+        var speech = new Audio(res.audio);
+        speech.play();
+        console.log(res);
+      });
+    };
     $scope.translate = function(params) {
       Translate.get({
         text: params.text,
