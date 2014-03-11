735fcb2accb97ca6370c7e510e0030b24f65d5d8
Howard Chung <no.c.l.unit@gmail.com>
2014-03-11 19:53:32 -0400

    Add GETters in lessons controller

--- app/controllers/lessons.js
+++ app/controllers/lessons.js
@@ -2,14 +2,33 @@
 
 var mongoose = require('mongoose');
 var Lesson = mongoose.model('Lesson');
-/*
+/**
   Get all lessons
 */
 exports.lessons = function(req, res) {
   Lesson
     .find({})
+    .select('fields')
     .exec(function(err, lessons) {
-      
+      var result = {};
+      if(err) 
+        result.err = err;
+      else
+        result = lessons;
+      res.send(result);
     });
 };
-  
+
+exports.entries = function(req, res) {
+  Lesson
+    .findOne({id: req.params.id})
+    .select('entries')
+    .exec(function(err, lesson) {
+      var result = {};
+      if(err)
+        result.err = err;
+      else
+        result = lesson;
+      res.send(result);
+    });
+};
