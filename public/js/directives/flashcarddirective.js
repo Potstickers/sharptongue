042d82bfcc78989c7372ac195031c1bd958cc499
Wellington Zhao <tuc16961@temple.edu>
2014-03-25 06:25:34 -0400

    add scope watch

--- public/js/directives/flashcarddirective.js
+++ public/js/directives/flashcarddirective.js
@@ -11,55 +11,58 @@ angular.module('sharptung.lessons').directive('flashcard', function(){
                     +"<i class='microphone-ico' />"
                   +"</button>"
                 +"</div>",
+    transclude: true,
     link: function(scope, elem, attrs) {
-      console.log(scope);
-      //init state
-      var lesson = scope.lesson;
-      var num_cards = lesson.entries.length;
-      var max_idx = num_cards - 1;
+      scope.$watch('lesson', function(lesson) {
+        if(lesson) {
+          console.log(lesson);
 
-      //bindings
-      elem.bind('click', flipCard);
-      elem.children()[1].find('button').bind('click', playTranslation);
-      angular.element(document.querySelector('LEFTBTN')).bind('click', prevCard);
-      angular.element(document.querySelector('RIGHTBTN')).bind('click', nextCard);
-      var cur_idx = 0;
-      var prev_idx = null;
-      var next_idx = (num_cards > 0)? 1 : null;
-      
-      function setScope() {
-        scope.entry = lesson.entries[cur_idx];
-      }
+          var setScope = function() {
+            scope.entry = lesson.entries[cur_idx];
+          };
 
-      function nextCard() {
-        if(cur_idx < max_idx) {
-          cur_idx++;
-        }else{
-          cur_idx = 0;
+          var nextCard = function() {
+            if(cur_idx < max_idx) {
+              cur_idx++;
+            }else{
+              cur_idx = 0;
+            }
+            setScope();
+          };
+
+          var prevCard = function() {
+            if(cur_idx > 0) {
+              cur_idx--;
+            }else{
+              cur_idx = max_idx;
+            } 
+            setScope();
+          };
+
+          var flipCard = function() {
+            elem.children()[0].toggleClass('VISIBLE'); //front
+            elem.children()[1].toggleClass('VISIBLE'); //back
+          }
+
+          var playTranslation = function() {
+            var speech = new Audio(lesson.entries[cur_idx].audioUrl); //cache this somewhere
+            speech.play();
+          }
+          //init state
+          var lesson = scope.lesson;
+          var num_cards = lesson.entries.length;
+          var max_idx = num_cards - 1;
+
+          //bindings
+          elem.bind('click', flipCard);
+          elem.children()[1].find('button').bind('click', playTranslation);
+          angular.element(document.querySelector('LEFTBTN')).bind('click', prevCard);
+          angular.element(document.querySelector('RIGHTBTN')).bind('click', nextCard);
+          var cur_idx = 0;
+          var prev_idx = null;
+          var next_idx = (num_cards > 0)? 1 : null;
         }
-        setScope();
-      }
-
-      function prevCard() {
-        if(cur_idx > 0) {
-          cur_idx--;
-        }else{
-          cur_idx = max_idx;
-        } 
-        setScope();
-      }
-
-      function flipCard() {
-        elem.children()[0].toggleClass('VISIBLE'); //front
-        elem.children()[1].toggleClass('VISIBLE'); //back
-      }
-
-      function playTranslation() {
-        var speech = new Audio(lesson.entries[cur_idx].audioUrl); //cache this somewhere
-        speech.play();
-      }
-
-
+      });
     }
   };
 });
