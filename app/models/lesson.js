f22ccdeb0cb4c22f3d3f41623fa81be1241c7bf5
Wellington Zhao <no.c.l.unit@gmail.com>
2014-03-25 10:12:11 -0400

    Remove maual create lesson

--- app/models/lesson.js
+++ app/models/lesson.js
@@ -35,17 +35,4 @@ LessonSchema.path('title').validate(function(title) {
     return title.length;
 }, 'Title cannot be blank');
 
-var Lesson = mongoose.model('Lesson', LessonSchema);
-Lesson.create({
-  title: "How to invalidate all arguments",
-  entries: [
-    {entry: "Do you even lift?", img:"lift?.jpg"},
-    {entry: "*Opposer looks in the mirror and realizes he doesn't lift.*", img:"selfreflect.jpg"},
-    {entry: "Opposer proclaims yes anyway.", img:"straightfacelier.png"},
-    {entry: "No you don't. No bicep peaks, quadricep? more like monocep... nvm thats just all fascia, here press this gallon o milk. *throws milk*", img:"litmustest.png"},
-    {entry: "Opposer catches milk and proceeds to press on it with index finger*", img:"doesntevenlift.png"},
-    {entry: "Done. *peace out*", img:"andalegendwasborned.png"}
-  ]
-}, function(err, lesson) {
-  console.log('Yay!');
-});
+mongoose.model('Lesson', LessonSchema);
