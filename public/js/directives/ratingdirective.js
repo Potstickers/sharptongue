6a195ea3dc87376867ffb8de0b659af966d8d581
Wellington Zhao <tuc16961@temple.edu>
2014-04-14 12:00:14 -0400

    add rating directive

---
+++ public/js/directives/ratingdirective.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('sharptung.lessons').directive('ratingBox', function() {
+  return {
+    templateUrl: 'tbd.html',
+    link: function(scope, elem, attrs) {
+      
+    }
+  };
+});
