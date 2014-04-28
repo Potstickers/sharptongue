330a23f7a321e3973411bee5565cd44533582af6
Wellington Zhao <no.c.l.unit@gmail.com>
2014-04-28 10:15:22 -0400

    frogger test

---
+++ public/js/directives/froggerdirective.js
@@ -0,0 +1,30 @@
+'use strict';
+angular.module('sharptung.lessons').directive('frogger', function(){
+  //reference: http://jsfiddle.net/eeuSv/
+  //reference: http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
+  console.log('in frogger directive');
+  return {
+    link: function(scope, elem, attrs) {
+      scope.$watch('fc', function(frog) {
+        if(frog) {
+          scope.frog.curEntry = {};
+          //init state
+          var entries;
+          var num_cards;
+          
+          var populateLangOpts = document.querySelector('[populate-lang-opts]');
+          
+          //init
+          var populateLangOptsChange = function(){
+            scope.frog.initFlashcards(angular.element(populateLangOpts).val(), function(lesson) {
+              entries = lesson.entries;
+              num_cards = entries.length;
+            });
+          };
+          populateLangOptsChange();
+          angular.element(populateLangOpts).change(populateLangOptsChange);
+        }
+      });
+    }
+  };
+});
